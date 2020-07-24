b2f791c5d84d32b647b7b4256df9c60f2859f594
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2020-07-24 13:22:59 +0530

    Add files via upload

---
+++ fronentend/build/precache-manifest.80cd3e4fd2345147ed591f0083dc7179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0cc1f73c59ec0f5a027615cf287bd1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3993365f180aca50d7a3",
+    "url": "/static/css/main.4bc37b21.chunk.css"
+  },
+  {
+    "revision": "442f0de896bc029c41cc",
+    "url": "/static/js/2.dd14d15b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd14d15b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3993365f180aca50d7a3",
+    "url": "/static/js/main.663e5602.chunk.js"
+  },
+  {
+    "revision": "4a27d8bc7032bd5b0aea",
+    "url": "/static/js/runtime-main.09efe011.js"
+  }
+]);
